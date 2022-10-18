8b229966e861d16ba5e960a71ac500f3bfdc970e
nzlv <nizaralfimuhamad@gmail.com>
2022-10-18 19:20:59 +0700

    belajar Controllers & routes

---
+++ app/Controllers/Admin/Users.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+use App\Controllers\BaseController;
+
+class Users extends BaseController
+{
+    public function index()
+    {
+        // return view('welcome_message');
+        // return view('keren');
+        echo "Ini pake Controller Admin, method Users, view index";
+    }
+}
